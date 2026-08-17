ca09fff0042fa673c23fadf9d2550bdfe3c2c54c
labyrt <labyrtcontact@gmail.com>
2026-08-17 12:48:48 -0300

    fix: ajusta logo do eFood conforme o Figma

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,19 +1,25 @@
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
-const LogoText = styled.span`
-  display: inline-block;
-  font-size: 34px;
-  line-height: 1;
-  font-weight: 900;
-  letter-spacing: -2px;
-  color: var(--cor-principal);
+import efoodLogo from '../../assets/efood-logo.png'
+
+const LogoLink = styled(Link)`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  line-height: 0;
+`
+
+const LogoImage = styled.img`
+  width: 94px;
+  height: 25px;
+  object-fit: contain;
 `
 
 const Logo = () => (
-  <Link to="/" aria-label="Ir para a página inicial do eFood">
-    <LogoText>efood</LogoText>
-  </Link>
+  <LogoLink to="/" aria-label="Ir para a página inicial do eFood">
+    <LogoImage src={efoodLogo} alt="eFood" />
+  </LogoLink>
 )
 
 export default Logo
