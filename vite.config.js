ebd5edb7dba50ae4c221dcacd703d05dcef4600c
labyrt <labyrtcontact@gmail.com>
2026-08-17 14:25:46 -0300

    chore: prepara base do eFood para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: process.env.GITHUB_PAGES === 'true' ? '/curso_ebac_frontend/' : '/'
 })
