acf0e4f809f2f47667d1ca9bef6d41abbbcc6281
labyrt <labyrtcontact@gmail.com>
2026-08-17 14:25:59 -0300

    chore: adapta roteamento do eFood ao GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 import App from './App'
 import GlobalStyle from './styles/GlobalStyle'
 
+const Router = import.meta.env.BASE_URL === '/' ? BrowserRouter : HashRouter
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <GlobalStyle />
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 )
