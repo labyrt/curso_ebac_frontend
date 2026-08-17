421206fdb425b50504a9b8bd0f6f42c3db4d33c9
labyrt <labyrtcontact@gmail.com>
2026-08-17 13:49:57 -0300

    fix: usa a logo oficial completa fornecida

--- src/components/BrandLogo/index.jsx
+++ src/components/BrandLogo/index.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
-import efoodWordmark from '../../assets/efood-logo.png'
+import { efoodLogoDataUri } from '../../assets/efood-logo-data-uri'
 
 const LogoLink = styled(Link)`
   display: inline-flex;
@@ -9,33 +9,18 @@ const LogoLink = styled(Link)`
   justify-content: center;
   width: 125px;
   height: 58px;
-  padding: 8px 9px;
-  border: 5px solid #e66767;
-  background: #fff;
   line-height: 0;
 `
 
-const Wordmark = styled.img`
-  width: 76px;
-  height: auto;
+const LogoImage = styled.img`
+  width: 125px;
+  height: 58px;
   object-fit: contain;
-`
-
-const Utensils = styled.svg`
-  width: 26px;
-  height: 34px;
-  margin-left: 3px;
-  flex: 0 0 auto;
-  fill: #e66767;
 `
 
 const BrandLogo = () => (
   <LogoLink to="/" aria-label="Ir para a página inicial do eFood">
-    <Wordmark src={efoodWordmark} alt="eFood" />
-    <Utensils viewBox="0 0 36 48" aria-hidden="true">
-      <path d="M2 1h3v13h3V1h3v13h3V1h3v16c0 5-3 8-7 9v20H7V26c-4-1-7-4-7-9V1h2z" />
-      <path d="M25 1c5 2 8 7 8 13v14h-5v18h-5V6c0-2 1-4 2-5z" />
-    </Utensils>
+    <LogoImage src={efoodLogoDataUri} alt="eFood" />
   </LogoLink>
 )
 
